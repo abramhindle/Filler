2c1007fafa3ec0d8df0e1de4b950e246b4584dfb
abez <abram.hindle@softwareprocess.es>
2010-08-10 20:26:56 -0400

    removed some common stuff

--- short/filler.js
+++ short/filler.js
@@ -20,22 +20,22 @@ while(i<s){
 }
 b[0]=0;
 b[s-1]=1;
-function pos(x,y) { return w * y + x }
-function drawBoard() {
+function pos(x,y){ return w * y + x }
+function drawBoard(){
   var x,y;
-  for(y=0;y<w;y++) {
-    for(x=0;x<w;x++) {
+  for(y=0;y<w;y++){
+    for(x=0;x<w;x++){
       var z=b[pos(x,y)];
       ctx.fillStyle=colors[z];
       ctx.fillRect(x*u,y*u,u,u);
     }
   }
 }
-function bounds(x,y) { return (x >= 0 && x < w && y >= 0 && y < w) }
-function h(b,xi,yi,c,rc) { //fillflood
-  if (!bounds(xi,yi)) { return 0; }
+function bounds(x,y){ return (x >= 0 && x < w && y >= 0 && y < w) }
+function h(b,xi,yi,c,rc){ //fillflood
+  if (!bounds(xi,yi)){ return 0; }
   var i=pos(xi,yi);
-  if (b[i]==c) {
+  if (b[i]==c){
     b[i]=rc;
     return 1+ 
            h(b,xi+1,yi,c,rc)+
@@ -46,25 +46,25 @@ function h(b,xi,yi,c,rc) { //fillflood
     return 0;
   }
 }
-function q(b, p, x, y, c) {
+function q(b,p,x,y,c){
   h(b,x,y,p,c);
   return h(b,x,y,c,p);
 }
-function ai(b,p,x,y,o,ox,oy) { 
+function ai(b,p,x,y,o,ox,oy){ 
   var choice=2,m=0,
-  helper=function(bd,p1,x1,y1,p2,x2,y2,d) {
+  helper=function(bd,p1,x1,y1,p2,x2,y2,d){
     var i=0,
     m=-s*s*s,//100000000;
     c=2;
-    for(i=2;i<6;i++) {
+    for(i=2;i<6;i++){
       var bi=bd.concat(),
       f=q(bi,p1,x1,y1,i);
-      if (d<3) {
+      if (d<3){
         var r = helper(bi,p2,x2,y2,p1,x1,y1,d+1);
         f = f - r[1];
 
       }
-      if (f>=m) {
+      if (f>=m){
         c=i;
         m=f;
       }      
@@ -78,19 +78,19 @@ function ai(b,p,x,y,o,ox,oy) {
 
 
 
-function onClick(e) {
+function onClick(e){
   var x=~~((e.clientX-ig.offsetLeft)/u),
       y=~~((e.clientY-ig.offsetTop)/u),
       i=pos(x,y),
       c=b[i];
-  if (c > 1) {
+  if (c > 1){
     // us
     var us = q( b, 0, 0, 0, c );
     // them
     var them = q( b, 1, w-1, w-1, ai(b,1,w-1,w-1,0,0,0) );
     //board[ i ] = 0;
-    if (us + them == s) {
-      if (us > them) {
+    if (us + them == s){
+      if (us > them){
         q(b,1,w-1,w-1,0);
       } else {
         q(b,1,w-1,w-1,0);
